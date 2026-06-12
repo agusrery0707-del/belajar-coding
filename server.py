1587b2213ff1e293c63370afcc735cc7b25e7f78
Agus <agusrery0707@gmail.com>
2026-06-12 08:10:27 +0700

    fix port untuk Railway

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask,jsonify,request 
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -75,5 +76,7 @@ def get_users():
         })
     return jsonify(hasil)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
